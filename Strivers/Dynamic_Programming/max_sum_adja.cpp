c931a9d9fef44fedb618a2b29c712e82769feeb1
Sreemanth-coder <sreemanthsree99@gmail.com>
2025-12-20 15:08:05 +0530

    Max_na_subseq

---
+++ Strivers/Dynamic_Programming/max_sum_adja.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sub(int ind,vector<int>&nums,vector<int>&dp){
+  if(ind<0) return 0;
+  if(ind ==0) return nums[0];
+  if(dp[ind]!=-1) return dp[ind];
+
+  int pick=nums[ind]+sub(ind-2,nums,dp);
+  int notpick=sub(ind-1,nums,dp);
+  return dp[ind]=max(pick,notpick);
+}
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> nums(n);
+  for(int i=0;i<n;i++){
+    cin>>nums[i];
+  }
+  vector<int> dp(n,-1);
+  sub(n-1,nums,dp);
+  cout<<dp[n-1];
+
+
+} 
