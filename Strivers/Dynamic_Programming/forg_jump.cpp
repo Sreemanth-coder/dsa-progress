9dd152450a39b54b3f30cf8ccd354a23364f8fcc
Sreemanth-coder <sreemanthsree99@gmail.com>
2025-12-20 13:55:26 +0530

    Done using tabulation method

--- Strivers/Dynamic_Programming/forg_jump.cpp
+++ Strivers/Dynamic_Programming/forg_jump.cpp
@@ -24,7 +24,21 @@ int main(){
     cin>>nums[i];
   }
   vector<int> dp(n,-1);
-  jump(n-1,nums,dp);
+  //jump(n-1,nums,dp);
+
+  //Tabulation Method
+  dp[0]=0;
+  for(int i=1;i<n;i++){
+    int j1=dp[i-1]+abs(nums[i]-nums[i-1]);
+    int j2=INT_MAX;
+    if(i>1){
+      j2=dp[i-2]+abs(nums[i]-nums[i-2]);
+    }
+    dp[i]=min(j1,j2);
+
+  }
+  cout<<dp[n-1];
+
   /* 
   Greedy doesnt work 
   int i=0;
@@ -52,5 +66,5 @@ int main(){
   }
   cout<<min;
   */
- cout<<dp[n-1]<<endl;
+ //cout<<dp[n-1]<<endl;
 }
