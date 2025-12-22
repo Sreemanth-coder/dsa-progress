fe1b2756df2237f4ad764b12d799e6fca9fd18e2
Sreemanth-coder <sreemanthsree99@gmail.com>
2025-12-22 22:36:19 +0530

    Unique paths using tabulation

--- Strivers/Dynamic_Programming/Unique_paths.cpp
+++ Strivers/Dynamic_Programming/Unique_paths.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+/*
 int path(int m,int n,vector<vector<int>> &dp){
  
   if(m==0 && n==0){
@@ -18,6 +18,7 @@ int path(int m,int n,vector<vector<int>> &dp){
   
   return dp[m][n]=up+left;
 }
+  */
 
 
 int main(){
@@ -25,8 +26,26 @@ int main(){
   cin>>m>>n;
   
   vector<vector<int>> dp(m,vector<int>(n,-1));
-  cout<<path(m-1,n-1,dp);
+  //cout<<path(m-1,n-1,dp);
   
+  for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+      if(i==0 && j==0){
+        dp[i][j]=1;
+        continue;
+      }
+      int up=0;
+      int down=0;
+      if(i>0 ){
+      up=dp[i-1][j];
+      }
+      if(j>0){
+        down=dp[i][j-1];
+      }
+      dp[i][j]=up+down;
+    }
+  }
+  cout<<dp[m-1][n-1];
 
 
 }
