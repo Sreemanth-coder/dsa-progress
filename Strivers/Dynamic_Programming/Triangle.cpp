ada0d61c232322127c074e9eab157e722c6368c9
Sreemanth-coder <sreemanthsree99@gmail.com>
2025-12-23 23:50:48 +0530

    Done

---
+++ Strivers/Dynamic_Programming/Triangle.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+int triangle(int ind,int ind2;vector<vector<int>> &tri){
+  //base case
+  if(ind==0) return tri[0][0];
+
+
+}
+*/
+
+
+
+int main(){
+  vector<vector<int>> tri={{2},{3,4},{6,5,7},{4,1,8,3}};
+  int n=tri.size();
+  int m=tri[n-1].size();
+  cout<<n<<m;
+
+ 
+
+}
