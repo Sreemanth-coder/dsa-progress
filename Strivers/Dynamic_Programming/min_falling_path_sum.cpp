6d38aa6c20783b2e41140e78f6ac25fca17e09f4
Sreemanth-coder <sreemanthsree99@gmail.com>
2025-12-24 01:43:20 +0530

    Falling_path

--- Strivers/Dynamic_Programming/min_falling_path_sum.cpp
+++ Strivers/Dynamic_Programming/min_falling_path_sum.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
 int path(int i,int j,vector<vector<int>> &nums,int n,vector<vector<int>> &dp){
   //BaseCase
   if(i==n-1) return nums[i][j];
@@ -14,7 +15,7 @@ int path(int i,int j,vector<vector<int>> &nums,int n,vector<vector<int>> &dp){
   
 
 }
-
+*/
 
 
 int main(){
@@ -27,10 +28,25 @@ int main(){
   int n=nums.size();
   int i=0,j=0;
   vector<vector<int>> dp(n,vector<int>(n,-1));
+  /*
   int mini=INT_MAX;
   for(int j=0;i<n;j++){
     mini=min(mini,path(i,j,nums,n,dp));
   }
-  cout<<mini;
-  
+ cout<<mini;
+  */
+ for(int j=0;j<nums[0].size();j++){
+  dp[n-1][j]=nums[n-1][j];
+ }
+ for(int i=n-2;i>=0;i--){
+  for(int j=0;j<nums[0].size();j++){
+    int same=dp[i+1][j];
+    int left=(j>0)?dp[i+1][j-1]:1e9;
+    int right=(j<nums[0].size()-1)?dp[i+1][j+1]:1e9;
+    dp[i][j]=nums[i][j]+min({same,left,right});
+    
+
+  }
+ }
+
 }
