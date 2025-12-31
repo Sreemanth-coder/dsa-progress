38c70788d1b6bc881fd0ed75c13da1e85339bd80
Sreemanth-coder <sreemanthsree99@gmail.com>
2025-12-31 12:27:47 +0530

    Min Coins using recursion

--- Strivers/Dynamic_Programming/Coin_change.cpp
+++ Strivers/Dynamic_Programming/Coin_change.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
 int cc(int ind,int amt,vector<int> &nums){
 
   if(ind==0) {
     return (amt % nums[0]==0);
   }
+  
   
   int not_pick=cc(ind-1,amt,nums);
   int pick=0;
@@ -28,3 +30,32 @@ int main(){
   cout<<cc(n-1,amt,nums);
 
 }
+  */
+
+  int ccl(int ind,int amt,vector<int>&nums){
+    if(ind==0){
+      if(amt%nums[0]==0) return amt/nums[0];
+      return 1e9;
+    }
+    int not_pick=0+ccl(ind-1,amt,nums);
+    int pick=INT_MAX;
+    if(nums[ind]<=amt){
+      pick=1+ccl(ind,amt-nums[ind],nums);
+    }
+    return min(pick,not_pick);
+
+    
+  }
+
+
+
+  int main(){
+    int n,amt;
+    cin>>n>>amt;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+      cin>>nums[i];
+    }
+    //vector<int>dp(n+1,-1);
+    cout<<ccl(n-1,amt,nums);
+  }
