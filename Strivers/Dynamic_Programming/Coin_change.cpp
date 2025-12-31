5c89c967da00b776ed8756ad1b6ddc7e0cdfab7f
Sreemanth-coder <sreemanthsree99@gmail.com>
2025-12-31 14:11:13 +0530

    Min Coinchange using tabulation

--- Strivers/Dynamic_Programming/Coin_change.cpp
+++ Strivers/Dynamic_Programming/Coin_change.cpp
@@ -32,6 +32,8 @@ int main(){
 }
   */
 
+  /*
+
   int ccl(int ind,int amt,vector<int>&nums,vector<vector<int>> &dp){
     if(ind==0){
       if(amt%nums[0]==0) return amt/nums[0];
@@ -47,6 +49,8 @@ int main(){
 
     
   }
+    */
+
 
 
 
@@ -58,5 +62,29 @@ int main(){
       cin>>nums[i];
     }
     vector<vector<int>>dp(n,vector<int>(amt+1,-1));
-    cout<<ccl(n-1,amt,nums,dp);
+    //cout<<ccl(n-1,amt,nums,dp);
+
+
+    //Tabulation
+    for(int i=0;i<amt+1;i++){
+      if(i%nums[0]==0) dp[0][i]=i/nums[0];
+      else dp[0][i]=1e9;
+    }
+    for(int i=0;i<n;i++){
+      dp[i][0]=1;
+    }
+    for(int i=1;i<n;i++){
+      for(int j=1;j<amt+1;j++){
+        int not_pick=dp[i-1][j];
+        int pick=1e9;
+        if(nums[i]<=j){
+          pick=1+dp[i][j-nums[i]];
+        }
+        dp[i][j]=min(pick,not_pick);
+      }
+    }
+    cout<<dp[n-1][amt];
+
+
+
   }
