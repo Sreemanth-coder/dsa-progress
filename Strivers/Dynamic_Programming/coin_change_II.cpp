8e0a0bfeb4d4a2e5dbde3a418964f925c7ad40ad
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-01-02 09:47:56 +0530

    Coin change_II using recursion

---
+++ Strivers/Dynamic_Programming/coin_change_II.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int cc(int ind,int amt,vector<int>&nums){
+ 
+  if(ind==0){
+    if(amt%nums[ind]==0) return 1;
+    return 0;
+  }
+  int not_pick=cc(ind-1,amt,nums);
+  int pick=0;
+  if(nums[ind]<=amt){
+    pick=cc(ind,amt-nums[ind],nums);
+  }
+  return pick+not_pick;
+}
+
+
+
+
+
+
+int main(){
+  int n;
+  cin>>n;
+  int amt;
+  cin>>amt;
+  vector<int> nums(n);
+  for(int i=0;i<n;i++){
+    cin>>nums[i];
+  }
+  cout<<cc(n-1,amt,nums);
+
+
+}
