386588334be4cd354d53ae1873aa5076976388b2
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-01-03 11:33:01 +0530

    Rod cutting using recursion

---
+++ Strivers/Dynamic_Programming/Rod_cutting_prb.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rcp(int ind,int N,vector<int>&nums){
+  if(ind==0){
+    return N*nums[0];
+  }
+  int not_pick=rcp(ind-1,N,nums);
+  int pick=INT_MIN;
+  if((ind+1)<=N){
+    pick=nums[ind]+rcp(ind,N-(ind+1),nums);
+  }
+  return max(pick,not_pick);
+
+}
+
+
+
+
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> nums(n);
+  for(int i=0;i<n;i++){
+    cin>>nums[i];
+  }
+  cout<<rcp(n-1,n,nums);
+
+
+}
