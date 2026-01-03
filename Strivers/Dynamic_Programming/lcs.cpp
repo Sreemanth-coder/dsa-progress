d15ce2f57c8f6636b6816b5aeefe334553a34926
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-01-03 14:53:36 +0530

    lcs using memo

--- Strivers/Dynamic_Programming/lcs.cpp
+++ Strivers/Dynamic_Programming/lcs.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 
-int lcs(int ind1,int ind2,string &s1,string &s2){
+int lcs(int ind1,int ind2,string &s1,string &s2,vector<vector<int>> &dp){
 
   if(ind1<0 || ind2<0) return 0;
+  if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
   
   if(s1[ind1]==s2[ind2]){
-    return 1+lcs(ind1-1,ind2-1,s1,s2);
+    return dp[ind1][ind2]=1+lcs(ind1-1,ind2-1,s1,s2,dp);
   }
-  return 0+max(lcs(ind1-1,ind2,s1,s2),lcs(ind1,ind2-1,s1,s2));
+  return dp[ind1][ind2]=max(lcs(ind1-1,ind2,s1,s2,dp),lcs(ind1,ind2-1,s1,s2,dp));
 
   
 
@@ -23,7 +24,8 @@ int main(){
   string s1,s2;
   cin>>s1>>s2;
   int ind1=s1.size()-1,ind2=s2.size()-1;
-  cout<<lcs(ind1,ind2,s1,s2);
+  vector<vector<int>> dp(s1.size(),vector<int>(s2.size(),-1));
+  cout<<lcs(ind1,ind2,s1,s2,dp);
 
 
 
