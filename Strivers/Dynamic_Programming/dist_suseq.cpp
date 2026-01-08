07de62425e5be89c201f2de2758c35d0533abb82
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-01-08 17:56:50 +0530

    Dis subseq using Tabulation

--- Strivers/Dynamic_Programming/dist_suseq.cpp
+++ Strivers/Dynamic_Programming/dist_suseq.cpp
@@ -23,8 +23,29 @@ int main(){
   string s,t;
   cin>>s>>t;
   int ind1=s.size(),ind2=t.size();
-  vector<vector<int>> dp(ind1,vector<int>(ind2,-1));
+  vector<vector<int>> dp(ind1+1,vector<int>(ind2+1,-1));
 
-  cout<<dis(ind1-1,ind2-1,s,t,dp);
+
+ // cout<<dis(ind1-1,ind2-1,s,t,dp);
+
+//Tabulation
+  for(int i=0;i<ind1+1;i++){
+    dp[i][0]=1;
+  }
+  for(int i=1;i<ind2+1;i++){
+    dp[0][i]=0;
+  }
+  for(int i=1;i<ind1+1;i++){
+    for(int j=1;i<ind2+1;j++){
+      if(s[i]==t[j]){
+        dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+      }
+      else{
+        dp[i][j]=dp[i-1][j];
+      }
+    }
+  }
+  cout<<dp[ind1][ind2];
+
 
 }
