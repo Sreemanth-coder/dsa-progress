6b6eb156415e4f6d92da2355c75a313210d279f7
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-01-08 17:16:51 +0530

    Dis subseq usign memo

--- Strivers/Dynamic_Programming/dist_suseq.cpp
+++ Strivers/Dynamic_Programming/dist_suseq.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 
-int dis(int i,int j,string s,string t){
-  if(i<0) return 0;
+int dis(int i,int j,string s,string t,vector<vector<int>> &dp){
   if(j<0) return 1;
+  if(i<0) return 0;
+  
+  
+  if(dp[i][j]!=-1) return dp[i][j];
 
   if(s[i]==t[j]){
-    return dis(i-1,j-1,s,t)+dis(i-1,j,s,t);
+    return dis(i-1,j-1,s,t,dp)+dis(i-1,j,s,t,dp);
   }
   else{
-    return dis(i-1,j,s,t);
+    return dis(i-1,j,s,t,dp);
   }
 
 }
@@ -19,7 +22,9 @@ int dis(int i,int j,string s,string t){
 int main(){
   string s,t;
   cin>>s>>t;
-  int ind1=s.size()-1,ind2=t.size()-1;
-  cout<<dis(ind1,ind2,s,t);
+  int ind1=s.size(),ind2=t.size();
+  vector<vector<int>> dp(ind1,vector<int>(ind2,-1));
+
+  cout<<dis(ind1-1,ind2-1,s,t,dp);
 
 }
