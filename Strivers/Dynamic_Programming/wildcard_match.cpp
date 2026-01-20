7f234b65d4a8b5e822a19661bd897b0f9baec211
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-01-20 12:34:10 +0530

    Wild card using memo updated

--- Strivers/Dynamic_Programming/wildcard_match.cpp
+++ Strivers/Dynamic_Programming/wildcard_match.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool wild(int i,int j,string &s1,string &s2,vector<vector<bool>> &dp){
+bool wild(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
 
   //base cases
   if(i<0 && j<0) return true;
   if(i<0 && j>=0) return false;
-  if(j<0){
-    for(int i1=0;i1<i;i1++){
+  if(j<0 && i>=0){
+    for(int i1=0;i1<=i;i1++){
       if(s1[i1]!='*') return false;
     }
     return true;
@@ -20,7 +20,7 @@ bool wild(int i,int j,string &s1,string &s2,vector<vector<bool>> &dp){
     return dp[i][j]=wild(i-1,j-1,s1,s2,dp);
   }
   if(s1[i]=='*'){
-    return dp[i][j]=wild(i,j-1,s1,s2,dp) || wild(i-1,j,s1,s2,dp);
+    return dp[i][j]=wild(i-1,j,s1,s2,dp) || wild(i,j-1,s1,s2,dp);
   }
   return dp[i][j]=false;
 
@@ -31,6 +31,6 @@ int main(){
   string s1,s2;
   cin>>s1>>s2;
   int n=s1.size(),m=s2.size();
-  vector<vector<bool>> dp(n,vector<bool>(m,-1));
+  vector<vector<int>> dp(n,vector<int>(m,-1));
   cout<<wild(n-1,m-1,s1,s2,dp);
 }
