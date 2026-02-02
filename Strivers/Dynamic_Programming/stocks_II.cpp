ac9787f167a3ad4e9c1c848dd17f8eb0ce2bec13
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-02-02 11:53:40 +0530

    stock II using memo

--- Strivers/Dynamic_Programming/stocks_II.cpp
+++ Strivers/Dynamic_Programming/stocks_II.cpp
@@ -1,22 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int m1(vector<int>&nums,int n,int ind,int buy,int price){
+int m1(vector<int>&nums,int n,int ind,int buy,int price,vector<vector<int>> &dp){
 
   //base case
   if(ind==n){
     return 0;
   }
+  if(dp[ind][buy]!=-1) return 0;
 
   if(buy==1){
-    price= max(-nums[ind]+m1(nums,n,ind+1,0,price),0+m1(nums,n,ind+1,1,price));
+    price= max(-nums[ind]+m1(nums,n,ind+1,0,price,dp),0+m1(nums,n,ind+1,1,price,dp));
 
   }
   else{
-    price=max(nums[ind]+m1(nums,n,ind+1,1,price),0+m1(nums,n,ind+1,0,price));
+    price=max(nums[ind]+m1(nums,n,ind+1,1,price,dp),0+m1(nums,n,ind+1,0,price,dp));
 
   }
-  return price;
+  return dp[ind][buy]=price;
 }
 
 
@@ -29,7 +30,8 @@ int main(){
     cin>>nums[i];
   }
   int price=0,buy=1,ind=0;
-  cout<<m1(nums,n,ind,buy,price);
+  vector<vector<int>> dp(n+1,vector<int>(2,-1));
+  cout<<m1(nums,n,ind,buy,price,dp);
 
 
 
