206684ff1c68efb040b9cdbc54ad61505c155f77
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-02-11 14:37:53 +0530

    LIS using Memoization

--- Strivers/Dynamic_Programming/LIS.cpp
+++ Strivers/Dynamic_Programming/LIS.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 
-int m1(int ind,int prev,vector<int>&nums){
+int m1(int ind,int prev,vector<int>&nums,vector<vector<int>>&dp){
 
   if(ind ==nums.size()) return 0;
+  if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
   
-  int len=0+m1(ind+1,prev,nums);
+  int len=0+m1(ind+1,prev,nums,dp);
   if(prev==-1 || nums[ind]>nums[prev]){
-    len=max(len,1+m1(ind+1,ind,nums));
+    len=max(len,1+m1(ind+1,ind,nums,dp));
   }
-  return len;
+  return dp[ind][prev+1]=len;
 
 }
 
@@ -23,6 +24,7 @@ int main(){
   }
   int ind=0;
   int prev=-1;
-  cout<<m1(ind,prev,nums); 
+  vector<vector<int>> dp(n,vector<int>(n+1,-1));
+  cout<<m1(ind,prev,nums,dp); 
 
 }
