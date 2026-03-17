30b95fc253a3359acb9670fa3b49807160e108f6
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-03-17 18:46:33 +0530

    Matrix Chain Multiplication using memoization

--- Strivers/Dynamic_Programming/Partition_DP/MCM.cpp
+++ Strivers/Dynamic_Programming/Partition_DP/MCM.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
 int mcm(int i,int j,vector<int>&nums){
   if(i==j) return 0;
   int mini=INT_MAX;
@@ -12,7 +13,21 @@ int mcm(int i,int j,vector<int>&nums){
   return mini;
 
 }
+  */
 
+  //using memoization
+int mcm(int i,int j,vector<int>&nums,vector<vector<int>> &dp){
+  if(i==j) return 0;
+  if(dp[i][j]!=-1) return dp[i][j];
+  int mini=INT_MAX;
+  int steps;
+  for(int k=i;k<j;k++){
+    steps=nums[i-1]*nums[k]*nums[j]+mcm(i,k,nums,dp)+mcm(k+1,j,nums,dp);
+  }
+  mini=min(mini,steps);
+  return dp[i][j]=mini;
+
+}
 
 
 
@@ -22,10 +37,11 @@ int main(){
   int n;
   cin>>n;
   vector<int> nums(n);
+  vector<vector<int>> dp(n,vector<int>(n,-1));
   for(int i=0;i<n;i++){
     cin>>nums[i];
   }
-  cout<<mcm(1,n-1,nums);
+  cout<<mcm(1,n-1,nums,dp);
 
 
 }
