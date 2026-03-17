c6fb8b6a8b056c4e167fb0be1d15c3da153d8a58
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-03-18 04:57:10 +0530

    MCM using tabulation

--- Strivers/Dynamic_Programming/Partition_DP/MCM.cpp
+++ Strivers/Dynamic_Programming/Partition_DP/MCM.cpp
@@ -41,7 +41,30 @@ int main(){
   for(int i=0;i<n;i++){
     cin>>nums[i];
   }
-  cout<<mcm(1,n-1,nums,dp);
+  //cout<<mcm(1,n-1,nums,dp);
+
+  //Tabulation
+  for(int i=0;i<n;i++){
+    dp[i][i]=0;
+  }
+  
+
+  for(int i=n-1;i>=1;i--){
+    for(int j=i+1;j<=n-1;j++){
+      int mini=1e9;
+      
+      for(int k=i;k<j;k++){
+        int steps=nums[i-1]*nums[k]*nums[j]+dp[i][k]+dp[k+1][j];
+        if(steps<mini)mini=steps;
+      }
+      
+      dp[i][j]=mini;
+    }
+
+  }
+  cout<<dp[1][n-1];
+  
+
 
 
 }
