b4d3a2aedd966acebc6ca59f36c2abfce5ff43a3
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-03-18 06:03:46 +0530

    Min Cost to cut using tabulation

--- Strivers/Dynamic_Programming/Partition_DP/min_cost_to_cut.cpp
+++ Strivers/Dynamic_Programming/Partition_DP/min_cost_to_cut.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*
  int cut(int i,int j,vector<int>&cuts,vector<vector<int>> &dp){
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
@@ -16,12 +17,36 @@ using namespace std;
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
         return cut(1,c,cuts,dp);
     }
+        */
+
     int main(){
       int n;
       cin>>n;
-      vector<int> cuts;
-      minCost(n,cuts);
+      int c;
+      cin>>c;
+      vector<int> cuts(c);
+      for(int i=0;i<c;i++){
+        cin>>cuts[i];
+      }
+      cuts.push_back(n);
+      cuts.insert(cuts.begin(),0);
+      sort(cuts.begin(),cuts.end());
+      //tabulation
+      vector<vector<int>> dp(c+2,vector<int>(c+2,0));
+      for(int i=c;i>=0;i--){
+        for(int j=1;j<=c;j++){
+          int mini=INT_MAX;
+          for(int k=i;k<=j;k++){
+            
+          
+            int cost=cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k-1][j];
+            mini=min(mini,cost);
+          }
+          dp[i][j]=mini;
+        }
+      }
+      cout<<dp[1][c];
     }
