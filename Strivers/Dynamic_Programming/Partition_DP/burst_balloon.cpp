34d617933c7683ecd1a4032ab0ca71122966e6ad
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-03-22 11:31:01 +0530

    Burst Balloons done using memoization

--- Strivers/Dynamic_Programming/Partition_DP/burst_balloon.cpp
+++ Strivers/Dynamic_Programming/Partition_DP/burst_balloon.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+/*
 
 int burst(int i,int j,vector<int>&nums){
   //base case
@@ -16,7 +16,22 @@ int burst(int i,int j,vector<int>&nums){
   return maxi;
 
 }
+*/
 
+//Memoization
+int burst(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
+  //base case
+  if(i>j) return 0;
+  if(dp[i][j]!=-1) return dp[i][j];
+  int maxi=INT_MIN;
+  for(int k=i;k<=j;k++){
+    int coins=nums[i-1]*nums[k]*nums[j+1]+burst(i,k-1,nums)+burst(k+1,j,nums);
+    maxi=max(maxi,coins);
+
+  }
+  return dp[i][j]=maxi;
+
+}
 
 
 int main(){
@@ -28,6 +43,7 @@ int main(){
   }
   nums.push_back(1);
   nums.insert(nums.begin(),1);
-  cout<<burst(1,n,nums);
+  vector<vector<int>> dp(n,vector<int>(n,-1));
+  cout<<burst(1,n,nums,dp);
 
 }
