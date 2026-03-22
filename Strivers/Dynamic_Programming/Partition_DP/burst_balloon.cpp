faedb96a57adbc8d89430dbbcd036e27b90974cf
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-03-22 11:51:20 +0530

    Burst Balloons done using tabulation

--- Strivers/Dynamic_Programming/Partition_DP/burst_balloon.cpp
+++ Strivers/Dynamic_Programming/Partition_DP/burst_balloon.cpp
@@ -17,7 +17,7 @@ int burst(int i,int j,vector<int>&nums){
 
 }
 */
-
+/*
 //Memoization
 int burst(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
   //base case
@@ -32,6 +32,7 @@ int burst(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
   return dp[i][j]=maxi;
 
 }
+  */
 
 
 int main(){
@@ -43,7 +44,22 @@ int main(){
   }
   nums.push_back(1);
   nums.insert(nums.begin(),1);
-  vector<vector<int>> dp(n,vector<int>(n,-1));
-  cout<<burst(1,n,nums,dp);
+  vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+  //cout<<burst(1,n,nums,dp);
+
+  //tabulation
+  for(int i=n;i>=1;i--){
+    for(int j=1;j<=n;j++){
+      if(i>j) continue;
+      int maxi=INT_MIN;
+      for(int k=i;k<=j;k++){
+        int coins=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j];
+        maxi=max(coins,maxi);
+
+      }
+      dp[i][j]=maxi;
+    }
+  }
+  cout<<dp[1][n];
 
 }
