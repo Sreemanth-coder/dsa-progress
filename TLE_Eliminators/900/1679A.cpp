d83cb42acaff8137b6024e4956b62f668b1c44d8
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-03-24 15:54:54 +0530

    Codeforces problem 1

--- TLE_Eliminators/900/1679A.cpp
+++ TLE_Eliminators/900/1679A.cpp
@@ -7,20 +7,18 @@ int  main(){
   while(t--){
     long long n;
     cin>>n;
-    if(n%2!=0){
+    if(n%2!=0 || n<4){
       cout<<-1<<endl;
       continue;
     }
-    long long max=0;
-    long long min=0;
-    if(n>=4){
-      max=n/4;
-
-    }
-    if(n>=6){
-      min=n/6;
-    }
+    long long max=1;
+    long long min=1;
     
+    max=n/4;
+    if(n>=6){
+      min=(n+5)/6;
+    }
+
     cout<<min<<" "<<max<<endl;
 
 
