0a4ea74c263881e9cbd447717946d1b4fea69c0b
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-04-12 18:40:14 +0530

    Todays progress

--- Strivers/Graphs/flood_fill.cpp
+++ Strivers/Graphs/flood_fill.cpp
@@ -5,30 +5,30 @@ int main(){
   vector<vector<int>> image={{1,1,1},{1,1,0},{1,0,1}};
   int color=2;
   int n=image.size();
-        int m=image[0].size();
-        
-        queue<pair<int,int>> q;
-        vector<vector<int>> vist=image;
-        q.push({sr,sc});
-        vist[sr][sc]=color;
+    int m=image[0].size();
+    
+    queue<pair<int,int>> q;
+    vector<vector<int>> vist=image;
+    q.push({sr,sc});
+    vist[sr][sc]=color;
 
-        int initialcolor=image[sr][sc];
-        if(initialcolor==color) return image;
+    int initialcolor=image[sr][sc];
+    if(initialcolor==color) return image;
 
-        int dx[]{-1,0,1,0};
-        int dy[]={0,1,0,-1};
-        while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nr=r+dx[i];
-                int nc=c+dy[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vist[nr][nc]!=color && image[nr][nc]==initialcolor){
-                    q.push({nr,nc});
-                    vist[nr][nc]=color;
-                }
+    int dx[]{-1,0,1,0};
+    int dy[]={0,1,0,-1};
+    while(!q.empty()){
+        int r=q.front().first;
+        int c=q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nr=r+dx[i];
+            int nc=c+dy[i];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && vist[nr][nc]!=color && image[nr][nc]==initialcolor){
+                q.push({nr,nc});
+                vist[nr][nc]=color;
             }
         }
-      
+    }
+    
 }
