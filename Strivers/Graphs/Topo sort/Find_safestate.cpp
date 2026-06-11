8b4b0f3d0a0135936828c4e1d10fc1bbd629f7bb
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-06-11 08:05:40 +0530

    Find eventual safe states using bfs + topo

--- Strivers/Graphs/Topo sort/Find_safestate.cpp
+++ Strivers/Graphs/Topo sort/Find_safestate.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 //dfs + cycle detection
-
+/*
 class Solution {
 
     public:
@@ -54,6 +54,56 @@ public:
                 final.push_back(i);
             }
         }
+        return final;
+        
+    }
+};
+
+*/
+class solution{
+
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<vector<int>> adj(n);
+       
+        //vector<int> vist(n,0);
+        //vector<int> pathvist(n,0);
+        //vector<int> check(n,0);
+        vector<int> final;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<graph[i].size();j++){
+                adj[graph[i][j]].push_back(i);
+            }
+        }
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<adj[i].size();j++){
+                indegree[adj[i][j]]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            final.push_back(node);
+            for(int it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        
+
+        sort(final.begin(),final.end());
         return final;
         
     }
