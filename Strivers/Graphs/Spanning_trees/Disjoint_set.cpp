632636d10f0fcea7fb468c2fcdd8cb5b7e3251e3
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-07-03 22:19:29 +0530

    Disjoint Set using size

--- Strivers/Graphs/Spanning_trees/Disjoint_set.cpp
+++ Strivers/Graphs/Spanning_trees/Disjoint_set.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 class DisjointSet{
-  vector<int> rank,parent;
+  vector<int> rank,parent,size;
 
 public:
   DisjointSet(int n){
     //assuming 1 based indexing
     rank.resize(n+1,0);
     parent.resize(n+1);
+    size.resize(n+1);
     for(int i=0;i<=n;i++){
       parent[i]=i;
+      size[i]=1;
     }
   }
 
@@ -36,15 +38,28 @@ public:
       rank[ult_pu]++;
     }
   }
+  void UnionbySize(int u,int v){
+    int ult_u=findUpar(u);
+    int ult_v=findUpar(v);
+    if(ult_u==ult_v) return;
+    if(size[ult_u]<size[ult_v]){
+      parent[ult_u]=ult_v;
+      size[ult_v]+=size[ult_u];
+    }
+    else{
+      parent[ult_v]=ult_u;
+      size[ult_u]+=size[ult_v];
+    }
+  }
 };
 
 int main(){
   DisjointSet ds(7);
-  ds.UnionbyRank(1,2);
-  ds.UnionbyRank(2,3);
-  ds.UnionbyRank(4,5);
-  ds.UnionbyRank(6,7);
-  ds.UnionbyRank(5,6);
+  ds.UnionbySize(1,2);
+  ds.UnionbySize(2,3);
+  ds.UnionbySize(4,5);
+  ds.UnionbySize(6,7);
+  ds.UnionbySize(5,6);
   if(ds.findUpar(3)==ds.findUpar(7)){
     cout<<"Same Comp";
   }
