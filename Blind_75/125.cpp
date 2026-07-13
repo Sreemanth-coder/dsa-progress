308d206565ba5a9849693a378c8218bfb1c97d8a
Sreemanth-coder <sreemanthsree99@gmail.com>
2026-07-13 17:16:51 +0530

    Valid palindrome question using two pointer

--- Blind_75/125.cpp
+++ Blind_75/125.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool isPalindrome(string s) {
@@ -14,3 +15,35 @@ public:
         
     }
 };
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="A man, a plan, a canal: Panama";
+    string result="";
+        
+      for(char c:s){
+        if(c>='A' && c<='Z'){
+            result+=tolower(c);
+        }
+        else if(c>='a' && c<='z'){
+            result+=c;
+        }
+      }
+      int i=0,j=result.size()-1;
+      while(i<=j){
+        if(result[i]==result[j]){
+            i++;
+            j--;
+        }
+        else{
+            cout<< false;
+        }
+
+      }
+      cout<< true;
+      for(char c:result){
+        cout<<c<<" ";
+      }
+        
+}
